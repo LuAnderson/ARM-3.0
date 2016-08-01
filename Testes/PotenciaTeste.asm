e5b5b8f035bc1fb607b0ca817052a4290c7ade71
crispaGM <crispimneto2010@hotmail.com>
2016-08-01 10:43:07 -0300

    Teste da potência com resultado no registrador de retorno

---
+++ Testes/PotenciaTeste.asm
@@ -0,0 +1,32 @@
+.data
+
+
+
+        .text
+
+main:   
+        
+        li $t0,3 #numero base
+        li $t1,4 # expoente    
+  
+ 
+                
+      
+        
+          
+        li $t2,0
+        move $t3,$t0
+
+        loop:
+      addi $t2,$t2,1
+    beq  $t2,$t1, exit
+   
+    mul $t0,$t0,$t3
+       J loop
+exit:
+
+    
+    
+    move $v0,$t0
+   
+   
