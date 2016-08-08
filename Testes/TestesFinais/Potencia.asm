fbb1703629eb10a2468fc24f0c60fab28e45b2c9
Lucas Anderson <lucasand96@gmail.com>
2016-08-08 12:48:11 -0300

    Update Potencia.asm

--- Testes/TestesFinais/Potencia.asm
+++ Testes/TestesFinais/Potencia.asm
@@ -1,18 +1,12 @@
 .data
 
-
-
         .text
 
 main:   
         
         li $t0,3 #numero base
         li $t1,4 # expoente    
-  
- 
-                
       
-        
           
         li $t2,0
         move $t3,$t0
@@ -24,8 +18,6 @@ main:
     mul $t0,$t0,$t3
        J loop
 exit:
-
-    
     
     move $a0,$t0
     
